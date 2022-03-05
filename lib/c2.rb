9fe137fffd284468f3cff56e1f21683fde1a83c5
supechicken <supechicken666@gmail.com>
2022-03-05 16:29:55 +0800

    init

--- lib/c2.rb
+++ lib/c2.rb
@@ -1,22 +1,30 @@
-# Parse c2 options
-
 require 'optparse'
 
-options = {}
-OptionParser.new do |opt|
-  opt.banner = "Usage: #{File.basename $0} [options]"
+cmdname = File.basename($0)
 
-  # help banner
-  opt.on '-h', '-?', '--help', "Prints this help" do
-    puts opt
-  end
+cmd_list = {
+  'install <pkgName>' => 'Install package(s)',
+  'remove  <pkgName>' => 'Remove package(s)',
+  'search  <pattern>' => 'Search package(s) based on <pattern>'
+}
 
-  opt.on '-v', '--[no-]verbose', "Be verbose" do |v|
-    options[:verbose] = v
-  end
+$opt = {}
+OptionParser.new do |parser|
+  parser.banner = "Options:"
 
-  opt.on '-V', '--version', "Print version and license information" do
-    require 'c2/misc/about'
-    puts CREW_VERSION_OUTPUT
-  end
+  # print corresponding info and exit immediately if the following params are given
+  parser.on '-h', '--help',         "Prints this help"                      { print_help; exit 0 }
+  parser.on '-V', '--version',      "Print version and license information" { print_info; exit 0 }
+  #################################################################################
+
+  parser.on '-v', '--[no-]verbose', 'Show extra information.' {|v| options[:verbose] = v }
 end.parse!
+
+# pass given params to command function
+func_name = "#{ARGV[0]}_cmd"
+
+if respond_to?(func_name.to_sym)
+  send(func_name, ARGV[1..-1])
+else
+  abort "Invaild command: #{ARGV[0]}".lightred
+end
--- lib/c2/misc/about.rb
+++ lib/c2/misc/about.rb
@@ -5,19 +5,22 @@ require 'rubygems'
 gemspec = Gem::Specification::load('chromebrew2.gemspec')
 
 # the output of any chromebrew2 executable's --version option
-CREW_VERSION_OUTPUT = <<~EOF
-  #{File.basename $0} (chromebrew2) #{gemspec.version}
+def print_info
+  puts <<~EOF
+    #{File.basename $0} (chromebrew2) #{gemspec.version}
 
-  Copyright (c) 2022 chromebrew2 Authors
-  This programCis free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, version 3 of the License.
+    Copyright (c) 2022 chromebrew2 Authors
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU General Public License for more details.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
 
-  You should have received a copy of the GNU General Public License
-  along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
-EOF
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+  EOF
+end
